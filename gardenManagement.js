1ce44c7fba7a00b929a03e85ad065cab13a09fcd
sande-swalah <alvinswalah@gmail.com>
2025-11-24 22:11:40 +0300

    still not fully finished

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,40 @@
+
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+function controlWatering(temperature) {
+temperature = 90
+
+    if ( temperature > 80) 
+        console.log( "Watering on" ) 
+    
+    else 
+        console.log( "Watering off" ) 
+controlWatering()
+
+}
+
+
+function timeOfDay ( ) {
+    timeOfDay= morning
+    if (timeOfDay = evening || night)
+        console.log( "Lights on" ); 
+    
+    else {
+        console.log ( "Lights off" );
+    }
+timeOfDay()
+}
+
+ function soilMoistureAdjustment ( soilMoisture) {
+ soilMoisture = 30
+
+    while ( soilMoisture < 40 ) 
+        console.log( soilMoisture );
+        soilMoisture = soilMoisture + 5
+    
+    console.log(soilMoisture)
+    let soilMoisture = 30
+soilMoistureAdjustment()
+ }
